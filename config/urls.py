3d862516e4aad1cb11e7a62f2bb17bbac9b4842d
Vladimir <BlackWiind@gmail.com>
2025-02-20 13:59:48 +1000

    Add simple-jwt authentication. Need change settings.py.

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,10 @@ from django.urls import path, include, re_path
 from .yasg import urlpatterns as doc_urls
 
 from config import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
@@ -12,6 +16,9 @@ urlpatterns = [
     path('', include('users.urls')),
     path('home/', include('reports.urls')),
     path('api-auth/', include('rest_framework.urls')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += doc_urls
