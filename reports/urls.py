332f291504945a1caa407f0e9b18172542b67b5d
Vladimir <BlackWiind@gmail.com>
2025-06-02 13:38:00 +1000

    sources of funding api hotfix.

--- reports/urls.py
+++ reports/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('feedback/', api.Feedback.as_view()),
     path('archive/', api.Archive.as_view()),
     path('canishutdownwaiting/', api.CanIShutDownWaiting.as_view()),
-    path('get-sources-of-funding/', api.SourcesOfFundingListView())
+    path('get-sources-of-funding/', api.SourcesOfFundingListView.as_view())
 
 ]
