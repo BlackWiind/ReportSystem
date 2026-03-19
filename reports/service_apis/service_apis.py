c1fb0a0c936b973906f2977fb3f18139adee3c83
Vladimir <BlackWiind@gmail.com>
2026-03-19 15:34:04 +1000

    hotfix. Пытаюсь фиксить баги в новом эндпоинте + добавил swagger schema

--- reports/service_apis/service_apis.py
+++ reports/service_apis/service_apis.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 
 from reports.models import Report
@@ -12,16 +14,26 @@ class DocumentDataForActions(APIView):
     """
     my_tags = ['Service_API', ]
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            required=['user_id', 'report_id'],
+            properties={
+                'user_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'report_id': openapi.Schema(type=openapi.TYPE_INTEGER)
+            }
+        ),
+    )
     def get(self, request):
+        print(request.GET)
         try:
             user_id = request.GET.get('user_id')
             document_id = request.GET.get('report_id')
         except:
-
             return JsonResponse(data={'message': f'Не предоставленны данные пользователя илт документа.'}, status=400)
         try:
-            document = Report.objects.filter(id=document_id)
-            user = User.objects.filter(id=user_id)
+            document = Report.objects.filter(id=document_id)[0]
+            user = User.objects.filter(id=user_id)[0]
         except ObjectDoesNotExist:
             return JsonResponse(data={'message': 'Несуществует такого пользователя или документа.'}, status=404)
 
