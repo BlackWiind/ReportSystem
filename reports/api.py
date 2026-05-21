9c785340e49c1898594d8f675601c6d47eb439e6
Vladimir <BlackWiind@gmail.com>
2026-05-21 10:13:34 +1000

    hotfix. Отключил пагинацию для списка статусов

--- reports/api.py
+++ reports/api.py
@@ -15,7 +15,7 @@ from rest_framework.views import APIView
 
 from reports.filters import ReportFilter
 from reports.mail import send_email
-from reports.models import Report, Tag, History, WaitingStatusForUser, SourcesOfFunding
+from reports.models import Report, Tag, History, SourcesOfFunding
 from reports.permissions import IsSuperuserOrReadOnly
 from reports.serializers import ReportRetrieveUpdateSerializer, DraftSerializer, \
     ReportCreateSerializer, TagsSerializer, ReportListSerializer, HistoryUpdateSerializer, \
@@ -283,6 +283,7 @@ class StatusesListApiView(generics.ListAPIView):
     serializer_class = StatusListSerializer
     permission_classes = [IsAuthenticated]
     my_tags = ['Other', ]
+    pagination_class = None
     queryset = Statuses.objects.all()
 
 
